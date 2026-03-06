5a962eb4179a505a0e1b1945b53379983d862049
Yogi Aprio <yogiaprio21@gmail.com>
2026-03-06 09:35:14 +0700

    update route in web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Merchant\ProfileController as MerchantProfileController;
 use App\Http\Controllers\Merchant\MenuController as MerchantMenuController;
